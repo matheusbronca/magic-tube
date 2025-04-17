c1ac8e92be1c394bcbddb62101d934e2e4cf9707
Matheus Bronca <matheus.bronca@frncubo.com>
2025-04-17 11:20:50 -0300

    feat: add postgres database and drizzle orm

---
+++ src/db/schema.ts
@@ -0,0 +1,22 @@
+import {
+  pgTable,
+  text,
+  uuid,
+  timestamp,
+  uniqueIndex,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const users = pgTable(
+  "users",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    clerkId: text("clerk_id").unique().notNull(),
+    name: varchar("name", { length: 255 }).notNull(),
+    // TODO: add banner fields
+    imageUrl: text("image_url").notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  },
+  (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)],
+);
