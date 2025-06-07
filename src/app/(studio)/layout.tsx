d833bbffbc82a04d74d86c2567b8080aefec3fb1
Matheus Bronca <matheus.bronca@frncubo.com>
2025-06-07 16:14:36 -0300

    feat: add metadata to studio page

--- src/app/(studio)/layout.tsx
+++ src/app/(studio)/layout.tsx
@@ -1,8 +1,13 @@
 import { StudioLayout } from "@/modules/studio/ui/layouts/studio-layout";
+import { Metadata } from "next";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
+
+export const metadata: Metadata = {
+  title: "Studio",
+};
 
 const Layout = ({ children }: LayoutProps) => {
   return <StudioLayout>{children}</StudioLayout>;
